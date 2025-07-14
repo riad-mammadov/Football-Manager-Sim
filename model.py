e8806f96ad90e0c9ff6732a52b9133f4fb7f9dd0
Riad <riad_mammadov@yahoo.com>
2025-07-14 19:42:48 +0100

    add comments

--- model.py
+++ model.py
@@ -7,6 +7,8 @@ class Player:
         self.rating = rating
         self.goals = 0
 
+# Match class with a simulate method, takes in a match instance and outputs a result based on team rating and random probability
+# ensuring that games arent completely one sided
 class Match:
     def __init__(self,team1,team2):
         self.team1 = team1
@@ -21,23 +23,31 @@ class Match:
         for player in self.team2.players:
             totalRating2 += player.rating
         
+        # stores average player rating for teams (can to be changed with transfers)
         average = {"home": totalRating1/len(self.team1.players), "away": totalRating2/len(self.team2.players)}
 
         home = average["home"]
         away = average["away"]
 
         homeProb = home / (home + away)       
+        awayProb = away / (away + home)
         
         x = random.random()
         drawProb = 0.15
 
+
+        # generates random float(0-1) and checks against the home probability.
+        # if x falls below home probability, home team wins, else away wins
         if x < drawProb:
             print(f"{self.team1.name} draws against {self.team2.name}")
         elif x < drawProb + homeProb * (1 - drawProb):
             print(f"{self.team1.name} wins against {self.team2.name}")
         else:
             print(f"{self.team2.name} wins against {self.team1.name}")
+        
 
+
+        print(f"{self.team1.name} : {home}, {self.team2.name} : {away}, {x}")
         
         
     
