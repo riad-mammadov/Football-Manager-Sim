79aa1ae2e0debc16cb9778e359500e0a00b46196
Riad <riad_mammadov@yahoo.com>
2025-07-14 19:11:28 +0100

    add sim logic

---
+++ class.py
@@ -0,0 +1,45 @@
+import random
+
+class Player:
+    def __init__(self,name,position,rating):
+        self.name = name
+        self.position = position
+        self.rating = rating
+
+class Match:
+    def __init__(self,team1,team2):
+        self.team1 = team1
+        self.team2 = team2
+    
+    def sim(self):
+        totalRating1 = 0
+        totalRating2 = 0
+
+        for player in self.team1.players:
+            totalRating1 += player.rating
+        for player in self.team2.players:
+            totalRating2 += player.rating
+        
+        average = {"home": totalRating1/len(self.team1.players), "away": totalRating2/len(self.team2.players)}
+
+        home = average["home"]
+        away = average["away"]
+
+        homeProb = home / (home + away)       
+        
+        x = random.random()
+        drawProb = 0.15
+
+        if x < drawProb:
+            print(f"{self.team1} draws against {self.team2}")
+        elif x < drawProb + homeProb * (1 - drawProb):
+            print(f"{self.team1} wins against {self.team2}")
+        else:
+            print(f"{self.team2} wins against {self.team1}")
+                
+
+class Team:
+    def __init__(self, name, players):
+        self.name = name
+        self.players = players
+        self.points = 0
