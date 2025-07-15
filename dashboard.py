f516d6d4ff8fc453497d13b1247aaa950f66aa2e
Riad <riad_mammadov@yahoo.com>
2025-07-15 21:18:35 +0100

    add main menu:

---
+++ dashboard.py
@@ -0,0 +1,32 @@
+def getDashboard():
+    for i, opt in enumerate(dashboard, 1):
+        print(f"{i}: {opt}")
+
+    while True:
+        try:
+            choice = int(input())
+            if i <= choice <= len(dashboard):
+                if choice == 1:
+                    return viewSquad()
+                elif choice == 2:
+                    return viewFixtures()
+                elif choice == 3:
+                    return playNext()
+                elif choice == 4:
+                    return handleQuit
+            else:
+                print("Enter a number within the range ")
+        except ValueError:
+            print("Please enter a valid number ")
+
+def viewSquad():
+    pass
+
+def viewFixtures():
+    pass
+
+def playNext():
+    pass
+
+def handleQuit():
+    pass
