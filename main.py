cc14fab47ab089c23dd97a5dd2e79f409db672d0
Riad <riad_mammadov@yahoo.com>
2025-07-17 15:24:54 +0100

    create League object, assign and generate fixtures

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-from classes.league import Player, Team, Match, Manager
+from classes.league import Player, Team, Match, Manager, League
 from setup import *
+from dashboard import *
 
 season = 0
 gamesPlayed = 0
 
-# if season == 0 and gamesPlayed == 0:
-#     name = input("Whats your name? ")
+def startGame():
+    name = input("Whats your name? ")
 
-#     for i,team in enumerate(teams, 1):
-#         print(f"{i}: {team}")
+    for i,team in enumerate(teams, 1):
+        print(f"{i}: {team}")
     
-#     while True:
-#         try:
-#             choice = int(input("What number team would you like to manage? "))
-#             if 1 <= choice <= len(teams):
-#                 selectedTeam = teams[choice-1]
-#                 break
-#             else:
-#                 print("Enter a number within the range ")
+    while True:
+        try:
+            choice = int(input("What number team would you like to manage? "))
+            if 1 <= choice <= len(teams):
+                selectedTeam = teams[choice-1]
+                break
+            else:
+                print("Enter a number within the range ")
             
-#         except ValueError:
-#             print("Please enter a valid number ")
+        except ValueError:
+            print("Please enter a valid number ")
+    
+    return Manager(name, selectedTeam)
+
+if season == 0 and gamesPlayed == 0:
+    prem = League("Premier League", teams)
+    prem.generateFixtures()
+    prem.assignSchedule()
+    user = startGame()
+    print(f"Welcome to {user.team.name}, {user.name}! We are really excited to have you here and cant wait for you to get started!")
+    getDashboard(user)
 
 
-# user = Manager(name, selectedTeam)
 
-# print(f"Welcome to {user.team.name}, {name}! We are really excited to have you here and cant wait for you to get started!")
 
-for i, opt in enumerate(dashboard, 1):
-    print(opt)
+
 
 
 # gw1 = Match(teams[4],teams[10])
