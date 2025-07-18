d91e65fed0e785390159ed36192b212925ba5295
Riad <riad_mammadov@yahoo.com>
2025-07-18 07:43:19 +0100

    add league object to each team object

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ gamesPlayed = 0
 
 def startGame():
     name = input("Whats your name? ")
-
+    print("\n")
     for i,team in enumerate(teams, 1):
         print(f"{i}: {team}")
     
     while True:
         try:
-            choice = int(input("What number team would you like to manage? "))
+            choice = int(input("\nWhat number team would you like to manage? "))
             if 1 <= choice <= len(teams):
                 selectedTeam = teams[choice-1]
                 break
             else:
                 print("Enter a number within the range ")
-            
+
         except ValueError:
             print("Please enter a valid number ")
     
@@ -27,15 +27,13 @@ def startGame():
 
 if season == 0 and gamesPlayed == 0:
     prem = League("Premier League", teams)
+    for team in teams:
+        team.league = prem
     prem.generateFixtures()
     prem.assignSchedule()
     user = startGame()
-    print(f"Welcome to {user.team.name}, {user.name}! We are really excited to have you here and cant wait for you to get started!")
-    getDashboard(user)
-
-
-
-
+    print(f"\nWelcome to {user.team.name}, {user.name}! We are really excited to have you here and cant wait for you to get started!")
+    loopDashboard(user)
 
 
 
